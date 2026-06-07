0e8ed140b54a04b2c387dbf219d617688124c78d
Samuel Pena <pena.sammy@gmail.com>
2026-06-07 07:23:25 -0700

    refactor(docs): simplify navbar component in layout by removing unnecessary links

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Footer, Layout, Link, Navbar } from "nextra-theme-docs";
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
@@ -13,19 +13,7 @@ export const metadata: Metadata = {
     "Product guide for Flashycardy users and developer documentation for contributors and API integrators.",
 };
 
-const navbar = (
-  <Navbar logo={<b>Flashycardy</b>} logoLink="/">
-    <Link href="/users/introduction" className="x:mr-4 x:max-md:hidden">
-      Product Guide
-    </Link>
-    <Link href="/developers/introduction" className="x:mr-4 x:max-md:hidden">
-      Developer Guide
-    </Link>
-    <Link href="/api/overview" className="x:max-md:hidden">
-      API
-    </Link>
-  </Navbar>
-);
+const navbar = <Navbar logo={<b>Flashycardy</b>} logoLink="/" />;
 
 const footer = <Footer>{`© ${new Date().getFullYear()} Flashycardy.`}</Footer>;
 
